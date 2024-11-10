26531a494c8669f14801427c8ac68ed1ea22e599
Felipe Bertoni <bertones.felipe@gmail.com>
2024-11-10 16:50:33 -0300

    update
    alert

--- NOVOSUNFLOWERS/models/Denuncia.js
+++ NOVOSUNFLOWERS/models/Denuncia.js
@@ -1,5 +1,7 @@
 const mongoose = require(`mongoose`)
 
+alert('entrou')
+
 const denunciaSchema = mongoose.Schema({
   content: { type: String, require: true },
   date: { type: Date, default: Date.now },
