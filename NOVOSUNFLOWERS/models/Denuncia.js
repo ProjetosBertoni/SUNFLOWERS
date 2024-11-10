ecf7f8b6963a6030b775ef294ead6579b9731519
Felipe Bertoni <bertones.felipe@gmail.com>
2024-11-10 16:52:55 -0300

    update
    voltei ao codigo normal

--- NOVOSUNFLOWERS/models/Denuncia.js
+++ NOVOSUNFLOWERS/models/Denuncia.js
@@ -1,7 +1,5 @@
 const mongoose = require(`mongoose`)
 
-alert('entrou')
-
 const denunciaSchema = mongoose.Schema({
   content: { type: String, require: true },
   date: { type: Date, default: Date.now },
